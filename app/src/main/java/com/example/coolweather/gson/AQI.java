842322a131849dd0922122e79a9509c2775309d1
Figure <601748054@qq.com>
2017-08-25 10:54:34 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,14 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by Administrator on 2017/8/23.
+ */
+
+public class AQI {
+    public class AQIcity{
+        public String api;
+        public String pm25;
+    }
+
+    public AQIcity city;
+}
